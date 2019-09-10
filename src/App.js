353e2765b76e45d586a7f2ab8afccbdde4fb7ea2
Richa Sharma <onlinericha19@gmail.com>
2019-09-10 12:56:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "ui container comments">
+      <ApprovalCard>
+        <CommentDetail 
+          author = "Sam" 
+          timeAgo = "Today at 4:30Pm" 
+          comment = "Nice Blog Post!" 
+          avatar = {faker.image.avatar()} />
+      </ApprovalCard>
+
+      <ApprovalCard>
+      
+      <CommentDetail 
+      author = "Alex" 
+      timeAgo = "Today at 5:30Pm" 
+      comment = "Great Blog Post!" 
+      avatar = {faker.image.avatar()} />
+
+      </ApprovalCard>
+
+     <ApprovalCard>
+     <CommentDetail  
+     author = "John" 
+     timeAgo = "Today at 6:30Pm" 
+     comment = "Nice Post!" 
+     avatar = {faker.image.avatar()}/>
+     </ApprovalCard>
     </div>
-  );
-}
+    );
+};
 
 export default App;
